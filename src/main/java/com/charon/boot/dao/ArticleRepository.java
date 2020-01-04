0cfca434096d06fa666bb55d9fbeccdb735bb1fb
tangk <tangk123456>
2020-01-04 19:10:39 +0800

    spring data jpa 分页和更新部分字段

--- src/main/java/com/charon/boot/dao/ArticleRepository.java
+++ src/main/java/com/charon/boot/dao/ArticleRepository.java
@@ -1,15 +1,26 @@
 package com.charon.boot.dao;
 
 import com.charon.boot.model.Article;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 /**
  * @description:
  * @author: charon
  * @create: 2020-01-04 16:05
  **/
-public interface ArticleRepository extends JpaRepository<Article,Long> {
+public interface ArticleRepository extends JpaRepository<Article,Long> , PagingAndSortingRepository<Article,Long> {
 
     // jPA会根据方法名自动生成SQL执行
     Article findByAuthor(String author);
+
+    // 查询article表的所有数据，传入Pageable分页参数
+    Page<Article> findAll(Pageable pageable);
+    //根据author字段查询article表数据，传入Pageable分页参数
+    Page<Article> findByAuthor(String author, Pageable pageable);
+    //根据author字段和title字段，查询article表数据，传入Pageable分页参数
+    Slice<Article> findByAuthorAndTitle(String author, String title, Pageable pageable);
 }
--- src/test/java/com/charon/boot/jpa/JPAKeyWordTest.java
+++ src/test/java/com/charon/boot/jpa/JPAKeyWordTest.java
@@ -2,12 +2,18 @@ package com.charon.boot.jpa;
 
 import com.charon.boot.dao.ArticleRepository;
 import com.charon.boot.model.Article;
+import com.charon.boot.utils.DynamicJPAUtils;
+import org.hibernate.annotations.DynamicUpdate;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.BeanUtils;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.*;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
+import java.util.List;
+import java.util.Optional;
 
 /**
  * @description: 对于find关键字进行测试
@@ -26,5 +32,28 @@ public class JPAKeyWordTest {
     public void userTest() {
         Article article = articleRepository.findByAuthor("charon");
         System.out.println(article);
+    }
+
+    @Test
+    public void pageTest(){
+        Pageable pageable = PageRequest.of(1, 3
+                , Sort.by("id").ascending().and(Sort.by("createTime").descending()));
+        Page<Article> articlePage = articleRepository.findAll(pageable);
+        List<Article> articleList = articlePage.getContent();
+        System.out.println(articleList);
+        Page<Article> byAuthor = articleRepository.findByAuthor("ss", pageable);
+        Slice<Article> byAuthorAndTitle = articleRepository.findByAuthorAndTitle("s", "s", pageable);
+    }
+
+    @Test
+    public void dynamicJPA(){
+        Article dynamicArticle =new Article();
+        dynamicArticle.setId(15L);
+        dynamicArticle.setAuthor("chacane");
+        dynamicArticle.setTitle("dynamicJPA");
+        Optional<Article> optionalArticle = articleRepository.findById(15L);
+        Article article = optionalArticle.get();
+        BeanUtils.copyProperties(dynamicArticle,article, DynamicJPAUtils.getNullPropertyNames(dynamicArticle));
+        articleRepository.save(article);
     }
 }
