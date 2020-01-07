65373f2d2384a782325b5e12b20894b43b1863af
tangk <tangk123456>
2020-01-07 21:45:56 +0800

    springboot一行代码rest风格

--- src/main/java/com/charon/boot/dao/ArticleDao.java
+++ src/main/java/com/charon/boot/dao/ArticleDao.java
@@ -2,12 +2,14 @@ package com.charon.boot.dao;
 
 import com.charon.boot.entity.Article;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 /**
  * @description:
  * @author: charon
  * @create: 2020-01-07 18:43
  **/
+@RepositoryRestResource(collectionResourceRel = "article",path="rest/article")
 public interface ArticleDao extends MongoRepository<Article,String> {
     Article findByAuthor(String author);
 }
