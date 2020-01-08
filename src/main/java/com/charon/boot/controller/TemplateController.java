ac76b15b6f4d72042870a5085e765b037e0535de
tangk <tangk123456>
2020-01-08 20:38:56 +0800

    springboot整合freemarker

---
+++ src/main/java/com/charon/boot/controller/TemplateController.java
@@ -0,0 +1,32 @@
+package com.charon.boot.controller;
+
+import com.charon.boot.entity.ArticleVO;
+import com.charon.boot.service.ArticleRestService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * @description:
+ * @author: charon
+ * @create: 2020-01-08 20:26
+ **/
+@Controller
+@RequestMapping("/template")
+public class TemplateController {
+
+    @Resource(name="articleMybatisRestServiceImpl")
+    ArticleRestService articleRestService;
+
+    @GetMapping("/freemarker")
+    public String index(Model model) {
+        List<ArticleVO> articles = articleRestService.getAll();
+        model.addAttribute("articles", articles);
+        //模版名称，实际的目录为：resources/templates/fremarkertemp.html
+        return "fremarkertemp";
+    }
+}
